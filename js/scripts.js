7e22851f005c2e2baf86dde74cc790b6f1770b5c
Dario Nicosia <47794748+DarioNicosia@users.noreply.github.com>
2019-06-08 20:06:52 +0100

    created single product page

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,9 @@ apiRequest.onreadystatechange= () => {
     const response = JSON.parse(apiRequest.response);
   
     
-    
+    function getAllCameras(){
     let mainContainer = document.getElementById("productMainPage");
+   
     for (let i = 0; i < response.length; i++){
         let img = document.createElement("img");
         let productName = document.createElement("h3");
@@ -21,6 +22,8 @@ apiRequest.onreadystatechange= () => {
         let totalProduct = document.createElement("div");
         let link = document.createElement("a");
 
+     
+
         productBottom.appendChild(productName);
         productBottom.appendChild(productPrice);
 
@@ -28,12 +31,15 @@ apiRequest.onreadystatechange= () => {
        
         link.classList.add("col-md-4"); 
         link.setAttribute("href", "product.html");
+        link.setAttribute("id", "single-Page-Link");
+        link.setAttribute("data-value", "response[i]._id");
         link.style.textDecoration = "none";
         link.style.color = "#232A34";
 
         img.setAttribute("src", response[i].imageUrl);
         productName.innerHTML = response[i].name;
         productPrice.innerHTML = response[i].price;
+       
         
 
 
@@ -41,14 +47,58 @@ apiRequest.onreadystatechange= () => {
         totalProduct.appendChild(productBottom);
         link.appendChild(totalProduct);
 
+       
+       
         mainContainer.appendChild(link);
+  
 
         
-    }
-    
+
+        };
+    };
+    getAllCameras();
+
+}
+};
+
+
+
+
+//single product page
+
+
+
+let secondApiRequest =  new XMLHttpRequest();
+secondApiRequest.onreadystatechange= () => {
+    if(secondApiRequest.readyState === 4){
+    const secondResponse = JSON.parse(secondApiRequest.response);
+  
+    let singleProductName = document.getElementById('single-product-name');
+
+    singleProductName.innerHTML = secondResponse.name;
+   
+        
     
 }
 };
+
+let singlePageCamera = document.getElementById("single-Page-Link");
+singlePageCamera.addEventListener('click', () =>{
+    let idCamera = singlePageCamera.dataset.dataValue
+    secondApiRequest.open('GET', 'http://localhost:3000/api/cameras?q='+idCamera);
+    secondApiRequest.send();
+    
+})
+
+
+
+
+
+
+
+
+
+
 
 
 

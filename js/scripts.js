deeda6d64ece6fae03791081db67fe10d067159a
Dario Nicosia <47794748+DarioNicosia@users.noreply.github.com>
2019-06-10 19:11:14 +0100

    add the single product page

--- js/scripts.js
+++ js/scripts.js
@@ -21,36 +21,38 @@ apiRequest.onreadystatechange= () => {
         let productBottom = document.createElement("div");
         let totalProduct = document.createElement("div");
         let link = document.createElement("a");
+        
 
      
-
+       
         productBottom.appendChild(productName);
         productBottom.appendChild(productPrice);
 
         productBottom.classList.add("product-bottom", "text-center");
-       
-        link.classList.add("col-md-4"); 
         link.setAttribute("href", "product.html");
-        link.setAttribute("id", "single-Page-Link");
-        link.setAttribute("data-value", "response[i]._id");
-        link.style.textDecoration = "none";
-        link.style.color = "#232A34";
+        totalProduct.classList.add("col-md-4"); 
+       
+        link.setAttribute("id", response[i]._id);
+        
+        
 
         img.setAttribute("src", response[i].imageUrl);
         productName.innerHTML = response[i].name;
-        productPrice.innerHTML = response[i].price;
-       
+        productPrice.innerHTML = 'price :' + response[i].price;
+        link.appendChild(img);
         
 
 
-        totalProduct.appendChild(img );
+        totalProduct.appendChild(link );
         totalProduct.appendChild(productBottom);
-        link.appendChild(totalProduct);
+        
 
        
        
-        mainContainer.appendChild(link);
-  
+        mainContainer.appendChild(totalProduct);
+        
+
+        
 
         
 
@@ -68,27 +70,49 @@ apiRequest.onreadystatechange= () => {
 
 
 
-let secondApiRequest =  new XMLHttpRequest();
+a.addEventListener ("click",($event) => {
+    $event.preventDefault();
+    //let idCamera = event.target.id;
+    let idCamera = document.getElementsByClassName("col-md-4").getAttribute("onclick");
+    let secondApiRequest =  new XMLHttpRequest();
+    secondApiRequest.onreadystatechange= () => {
+    if(secondApiRequest.readyState === 4){
+    const secondResponse = JSON.parse(secondApiRequest.response);
+    let singleProductName = document.getElementById('single-product-name');
+    singleProductName.innerHTML = secondResponse.name;
+    }
+    };
+    secondApiRequest.open('GET', 'http://localhost:3000/api/cameras?q='+ idCamera);
+    secondApiRequest.send();
+    })
+
+
+
+
+
+
+
+
+/*let secondApiRequest =  new XMLHttpRequest();
 secondApiRequest.onreadystatechange= () => {
     if(secondApiRequest.readyState === 4){
     const secondResponse = JSON.parse(secondApiRequest.response);
-  
     let singleProductName = document.getElementById('single-product-name');
-
     singleProductName.innerHTML = secondResponse.name;
-   
-        
-    
-}
+   }
 };
 
-let singlePageCamera = document.getElementById("single-Page-Link");
-singlePageCamera.addEventListener('click', () =>{
-    let idCamera = singlePageCamera.dataset.dataValue
-    secondApiRequest.open('GET', 'http://localhost:3000/api/cameras?q='+idCamera);
-    secondApiRequest.send();
+secondApiRequest.open('GET', 'http://localhost:3000/api/cameras/5be9c4471c9d440000a730e8');
+secondApiRequest.send();
+
+
+let singlePageCamera = document.getElementsByClassName("single-product-name"); 
+singlePageCamera.addEventListener ("click",()=> {
     
-})
+})*/
+
+
+
 
 
 
